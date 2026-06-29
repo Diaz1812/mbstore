8d84df3cf745ed2f9bd39517ac1fa11e6232fff0
Muhammad Diaz Alwi <alwidiaz502@gmail.com>
2026-06-30 02:34:58 +0700

    Initial Laravel setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+
+    <title>MB STORE</title>
+
+    @vite(['resources/css/app.css','resources/js/app.js'])
+
+</head>
+
+<body class="bg-[#2933B1] text-white">
+
+    @yield('content')
+
+</body>
+</html>
---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="flex min-h-screen items-center justify-center">
+
+    <div class="text-center">
+
+        <h1 class="text-5xl font-bold">
+            MB STORE
+        </h1>
+
+        <p class="mt-3 text-white/80">
+            Trusted Game Account Marketplace
+        </p>
+
+    </div>
+
+</div>
+
+@endsection
